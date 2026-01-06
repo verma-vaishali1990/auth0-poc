4eb2ae2d949581b0dcfde929ffba0fadc421f6ef
Vaishali <zz_vermav.ext@fivebelow.com>
2026-01-06 16:37:44 +0530

    add changes for auth poc

---
+++ src/auth/lock.js
@@ -0,0 +1,19 @@
+var passwordlessOptions = {
+  allowedConnections: ['email'],
+  passwordlessMethod: 'code',
+  auth: {
+    redirect: false,
+    responseType: 'token id_token',
+    params: {
+      scope: 'openid email'               
+    }          
+  }
+}
+
+var lock = new window.Auth0LockPasswordless(
+  process.env.REACT_APP_AUTH0_CLIENT_ID,
+  process.env.REACT_APP_AUTH0_DOMAIN,
+ passwordlessOptions
+);
+
+export default lock;
